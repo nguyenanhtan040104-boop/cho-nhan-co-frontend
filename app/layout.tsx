df30111f9232372c270aecfa6614e9bfb7a5f0be
Nguyen Anh Tan <nguyenanhtan@gmail.com>
2026-05-18 14:36:50 +0700

    feat: add custom SVG favicon - green shop icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/icon.svg" />
         {/* Structured data - Organization */}
         <script
           type="application/ld+json"
